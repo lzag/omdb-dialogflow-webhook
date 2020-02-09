31e6d6e9a0d7225e06d5bf6105da1c9618e8b4b4
Lukasz Zagroba <34895155+lzag@users.noreply.github.com>
2020-02-09 12:52:49 +0100

    Adds basic authentication

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const basicAuth = require('express-basic-auth');
 
 const app = express();
 
+app.use(basicAuth({
+  users: {
+    [process.env.AUTH_USER]: process.env.AUTH_PASS,
+  },
+}));
+
 app.use('/', (req, res) => {
-  res.send('Hello World!');
+  if (req.header('x-dialogflow-validation') === process.env.AUTH_HEADER) {
+    res.send('Hello World!');
+  }
 });
 
 
